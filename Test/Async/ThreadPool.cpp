99176eed1fdbeed93534f5c33e69d0dc60817360
mocabe <hisa.mocabe@gmail.com>
2025-03-03 01:51:58 +0900

    Fix test

--- Test/Async/ThreadPool.cpp
+++ Test/Async/ThreadPool.cpp
@@ -4,41 +4,13 @@
 #include <Futurewalker.Async.AsyncFunction.hpp>
 #include <Futurewalker.Async.Task.hpp>
 
-#include <Futurewalker.Base.Win.PlatformDebugWin.hpp>
-
 #include <thread>
-#include <iostream>
 
 using namespace Futurewalker;
 
-class PlatformDebugTest : public PlatformDebug
-{
-public:
-    auto Print(const StringView message) -> void override
-    {
-        auto str = std::string((const char*)(const char8_t*)message.GetData(), (size_t)message.GetSize());
-        std::cerr << str << std::endl;
-    }
-
-    auto Break() -> void override
-    {
-    }
-};
-
-template <>
-struct Locator::Resolver<PlatformDebugTest>
-{
-    using Interface = PlatformDebug;
-    static auto Resolve() -> Shared<PlatformDebugTest>
-    {
-        return Shared<PlatformDebugTest>::Make();
-    }
-};
-
 TEST_CASE("ThreadPool")
 {
-    auto debug = Locator::ResolveWithDefault<PlatformDebug, PlatformDebugTest>();
-    auto pool = Locator::ResolveWithDefault<ThreadPool>();
+    const auto pool = Locator::ResolveWithDefault<ThreadPool>();
 
     SECTION("Schedule")
     {
