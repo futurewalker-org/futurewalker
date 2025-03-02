ac99c4a8ce0ee8eefcf363175683933c4ba74182
mocabe <hisa.mocabe@gmail.com>
2025-03-02 16:34:57 +0900

    Add wrapper types

---
+++ Source/Core/Futurewalker.Core.Function.hpp
@@ -0,0 +1,21 @@
+﻿// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Framework.Core.Prelude.hpp"
+
+#include <functional>
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Wrapper for function type.
+///
+/// TODO: Use `std::copyable_function` in C++26.
+/// https://en.cppreference.com/w/cpp/utility/functional/copyable_function
+///
+template <class F>
+using Function = std::function<F>;
+}
+}
---
+++ Source/Core/Futurewalker.Core.HashMap.hpp
@@ -0,0 +1,20 @@
+﻿// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Futurewalker.Core.Prelude.hpp"
+
+#include <unordered_map>
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Wrapper for hash map.
+///
+/// TODO: Choose best one from std, boost, abseil, etc.
+///
+template <class K, class V, class H = std::hash<K>, class P = std::equal_to<K>, class A = std::allocator<std::pair<const K, V>>>
+using HashMap = std::unordered_map<K, V, H, P, A>;
+}
+}
