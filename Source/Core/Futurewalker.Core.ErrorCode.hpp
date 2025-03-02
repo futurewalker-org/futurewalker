8feb9769a3fcbe618fe3145aa2968dfbf994b702
mocabe <hisa.mocabe@gmail.com>
2025-03-02 13:59:37 +0900

    Add ErrorCode

---
+++ Source/Core/Futurewalker.Core.ErrorCode.hpp
@@ -0,0 +1,25 @@
+﻿// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Futurewalker.Core.Prelude.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Error code enum.
+///
+/// Common error codes which can be useful for diagnostic purposes.
+///
+enum class ErrorCode
+{
+    Success = 0,         // Success.
+    Failure,             // Generic failure.
+    InvalidArgument,     // Arguments provided were invalid.
+    InvalidOperation,    // Invalid operation.
+    ResourceUnavailable, // Resource unavailable.
+    OperationAborted,    // Operation aborted.
+};
+}
+}
