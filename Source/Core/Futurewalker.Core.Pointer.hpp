d123791a41410d50caeeeddc538e5bdadd51441f
mocabe <hisa.mocabe@gmail.com>
2025-03-02 13:57:32 +0900

    Add Pointer

---
+++ Source/Core/Futurewalker.Core.Pointer.hpp
@@ -0,0 +1,333 @@
+﻿// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Futurewalker.Core.Prelude.hpp"
+#include "Futurewalker.Core.Concepts.hpp"
+#include "Futurewalker.Core.Primitive.hpp"
+#include "Futurewalker.Core.DynamicCastFunction.hpp"
+
+#include <boost/operators.hpp>
+#include <boost/functional/hash.hpp>
+
+#include <functional>
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Safe pointer wrapper.
+///
+/// Behaves like T* raw pointers, but cannot be default constructed.
+///
+template <class T>
+class Pointer 
+{
+    template <class U>
+    friend class Pointer;
+
+public:
+    Pointer() = delete;
+    Pointer(const Pointer&) noexcept = default;
+    Pointer& operator=(const Pointer&) noexcept = default;
+
+    ///
+    /// @brief Construct from other Pointer.
+    ///
+    template <class U>
+    requires Concepts::ImplicitlyConvertibleTo<U*, T*>
+    constexpr Pointer(Pointer<U> other) noexcept
+      : _ptr {other._ptr}
+    {
+    }
+
+    ///
+    /// @brief Assign from other Pointer.
+    ///
+    template <class U>
+    requires Concepts::ImplicitlyConvertibleTo<U*, T*>
+    constexpr auto operator=(Pointer<U> other) noexcept -> Pointer&
+    {
+        _ptr = other._ptr;
+        return *this;
+    }
+
+    ///
+    /// @brief Construct from nullptr_t.
+    ///
+    /// @note Uses constrained template parameter to avoid implicit conversion to nullptr_t.
+    ///
+    template <class U>
+    requires Concepts::SameAs<U, std::nullptr_t>
+    constexpr Pointer(U) noexcept
+      : _ptr {nullptr}
+    {
+    }
+
+    ///
+    /// @brief Assign null pointer.
+    ///
+    template <class U>
+    requires Concepts::SameAs<U, std::nullptr_t>
+    constexpr auto operator=(U) noexcept -> Pointer&
+    {
+        _ptr = nullptr;
+        return *this;
+    }
+
+    ///
+    /// @brief Construct from raw pointer.
+    ///
+    template <Concepts::Pointer U>
+    requires Concepts::ImplicitlyConvertibleTo<U, T*>
+    constexpr Pointer(U ptr) noexcept
+      : _ptr {ptr}
+    {
+    }
+
+    ///
+    /// @brief Assign null pointer.
+    ///
+    template <Concepts::Pointer U>
+    requires Concepts::ImplicitlyConvertibleTo<U, T*>
+    constexpr auto operator=(U ptr) noexcept -> Pointer&
+    {
+        _ptr = ptr;
+        return *this;
+    }
+
+    ///
+    /// @brief Convert to pointer type.
+    ///
+    template <Concepts::Pointer U>
+    requires Concepts::ImplicitlyConvertibleTo<T*, U>
+    constexpr explicit operator U() const noexcept
+    {
+        return static_cast<U>(_ptr);
+    }
+
+    ///
+    /// @brief Convert to other Pointer type.
+    ///
+    template <class U>
+    requires (!Concepts::ImplicitlyConvertibleTo<T*, U*>) && Concepts::ExplicitlyConvertibleTo<T*, U*>
+    constexpr explicit operator Pointer<U>() const noexcept
+    {
+        return Pointer<U>(static_cast<U*>(_ptr));
+    }
+
+    ///
+    /// @brief operator bool 
+    ///
+    constexpr explicit operator bool() const noexcept
+    {
+        return static_cast<bool>(_ptr);
+    }
+
+    ///
+    /// @brief operator*
+    ///
+    constexpr auto operator*() const noexcept -> auto&
+    requires Concepts::NonVoid<T>
+    {
+        return *_ptr;
+    }
+
+    ///
+    /// @brief operator->
+    ///
+    constexpr auto operator->() const noexcept -> T*
+    {
+        return _ptr;
+    }
+
+    ///
+    /// @brief operator+=
+    ///
+    constexpr auto operator+=(const SInt64 x) noexcept -> Pointer&
+    {
+        _ptr += static_cast<ptrdiff_t>(x);
+        return *this;
+    }
+
+    ///
+    /// @brief operator-=
+    ///
+    constexpr auto operator-=(const SInt64 x) noexcept -> Pointer&
+    {
+        _ptr -= static_cast<ptrdiff_t>(x);
+        return *this;
+    }
+
+    ///
+    /// @brief operator++
+    ///
+    constexpr auto operator++() noexcept -> Pointer&
+    {
+        ++_ptr;
+        return *this;
+    }
+
+    ///
+    /// @brief operator--
+    ///
+    constexpr auto operator--() noexcept -> Pointer&
+    {
+        --_ptr;
+        return *this;
+    }
+
+    ///
+    /// @brief operator++
+    ///
+    constexpr auto operator++(int) noexcept -> Pointer
+    {
+        const auto tmp = *this;
+        ++_ptr;
+        return tmp;
+    }
+
+    ///
+    /// @brief operator--
+    ///
+    constexpr auto operator--(int) noexcept -> Pointer
+    {
+        const auto tmp = *this;
+        --_ptr;
+        return tmp;
+    }
+
+    ///
+    /// @brief operator[]
+    ///
+    constexpr auto operator[](const SInt64 d) const noexcept -> auto&
+    requires Concepts::NonVoid<T>
+    {
+        return _ptr[static_cast<ptrdiff_t>(d)];
+    }
+
+    ///
+    /// @brief operator()
+    ///
+    template <class... Args>
+    constexpr auto operator()(Args&&...args) const
+    {
+        return _ptr(std::forward<Args>(args)...);
+    }
+
+    ///
+    /// @brief operator<=>
+    ///
+    friend constexpr auto operator<=>(const Pointer& l, const Pointer& r) noexcept -> std::strong_ordering
+    {
+        return l._ptr <=> r._ptr;
+    }
+
+    ///
+    /// @brief operator==
+    ///
+    friend constexpr bool operator==(const Pointer& l, const Pointer& r) noexcept
+    {
+        return l._ptr == r._ptr;
+    }
+
+    ///
+    /// @brief operator+
+    ///
+    friend constexpr auto operator+(const Pointer& ptr, const SInt64 x) noexcept -> Pointer
+    {
+        auto tmp = ptr;
+        tmp += x;
+        return tmp;
+    }
+
+    ///
+    /// @brief operator-
+    ///
+    friend constexpr auto operator-(const Pointer& ptr, const SInt64 x) noexcept -> Pointer
+    {
+        auto tmp = ptr;
+        tmp -= x;
+        return tmp;
+    }
+
+    ///
+    /// @brief Check if the holding object is instance of a type.
+    ///
+    /// @tparam U A type to check.
+    ///
+    /// @note Null returns false for all type of U.
+    ///
+    template <class U>
+    auto Is() const noexcept -> Bool
+    {
+        return DynamicCastFunction::Is<U>(_ptr);
+    }
+
+    ///
+    /// @brief Cast the pointer into another type.
+    ///
+    /// @tparam U A type to cast into.
+    ///
+    /// @return
+    ///
+    /// @throw Exception when owning object is not an instance of U.
+    ///
+    template <class U>
+    auto As() const -> Pointer<U>
+    {
+        return Pointer<U>(DynamicCastFunction::As<U>(_ptr));
+    }
+
+    ///
+    /// @brief Cast the pointer into another type, or return null on failure.
+    ///
+    /// @tparam U
+    ///
+    /// @return
+    ///
+    template <class U>
+    auto Maybe() const noexcept -> Pointer<U>
+    {
+        return Pointer<U>(DynamicCastFunction::Maybe<U>(_ptr));
+    }
+
+    ///
+    /// @brief Cast the pointer into another type without any runtime check.
+    ///
+    /// @tparam U
+    ///
+    /// @return
+    ///
+    template <class U>
+    auto Assume() const noexcept -> Pointer<U>
+    {
+        return Pointer<U>(DynamicCastFunction::Assume<U>(_ptr));
+    }
+
+private:
+    T* _ptr;
+};
+
+template <class T>
+Pointer(T*) -> Pointer<T>;
+}
+}
+
+template <class T>
+struct std::hash<FW_NS::Pointer<T>>
+{
+    constexpr auto operator()(const FW_NS::Pointer<T>& p) const noexcept -> size_t
+    {
+        return std::hash<T*>()(static_cast<T*>(p));
+    }
+};
+
+template <class T>
+struct boost::hash<FW_NS::Pointer<T>>
+{
+    constexpr auto operator()(const FW_NS::Pointer<T>& p) const noexcept -> size_t
+    {
+        return std::hash<T*>()(static_cast<T*>(p));
+    }
+};
---
+++ Test/Core/Pointer.cpp
@@ -0,0 +1,106 @@
+﻿#include <catch2/catch_all.hpp>
+
+#include <Futurewalker.Core.Pointer.hpp>
+
+#include <cstdint>
+#include <memory>
+
+using namespace Futurewalker;
+
+TEST_CASE("Pointer")
+{
+    STATIC_REQUIRE(!std::is_default_constructible_v<Pointer<const char>>);
+
+    SECTION("Construct from nullptr")
+    {
+        constexpr Pointer<const char> p1 = nullptr;
+        //constexpr Pointer<const char> p2 = 0; // should not work.
+        //constexpr Pointer<const char> p2 = false; // should not work.
+        REQUIRE(!p1);
+        STATIC_REQUIRE(!Concepts::ImplicitlyConvertibleTo<int, Pointer<const char>>);
+        STATIC_REQUIRE(!Concepts::ImplicitlyConvertibleTo<bool, Pointer<const char>>);
+    }
+
+    SECTION("Assign from nullptr")
+    {
+        Pointer<const char> p = nullptr;
+        REQUIRE(!p);
+
+        p = nullptr;
+        REQUIRE(!p);
+    }
+
+    SECTION("Construct from pointer")
+    {
+        const char* s1 = "1";
+        const char* s2 = "2";
+
+        Pointer<const char> p = s1;
+        REQUIRE(p);
+        REQUIRE(p == s1);
+
+        p = s2;
+        REQUIRE(p);
+        REQUIRE(p == s2);
+    }
+
+    SECTION("Construct from string literal")
+    {
+        Pointer<const char> p = "str";
+        REQUIRE(p);
+    }
+
+    SECTION("Comparison")
+    {
+        Pointer<const char> p = "str";
+        REQUIRE(p == p);
+        p = nullptr;
+        REQUIRE(p == p);
+    }
+
+    SECTION("Const conversion")
+    {
+        STATIC_REQUIRE(Concepts::ImplicitlyConvertibleTo<Pointer<char>, Pointer<const char>>);
+        STATIC_REQUIRE(!Concepts::ImplicitlyConvertibleTo<Pointer<const char>, Pointer<char>>);
+        STATIC_REQUIRE(Concepts::ImplicitlyConvertibleTo<Pointer<char>, Pointer<void>>);
+        STATIC_REQUIRE(Concepts::ImplicitlyConvertibleTo<Pointer<char>, Pointer<const void>>);
+
+        STATIC_REQUIRE(!Concepts::ImplicitlyConvertibleTo<Pointer<void>, Pointer<char>>);
+        STATIC_REQUIRE(!Concepts::ImplicitlyConvertibleTo<Pointer<void>, Pointer<const char>>);
+        STATIC_REQUIRE(Concepts::ExplicitlyConvertibleTo<Pointer<void>, Pointer<char>>);
+        STATIC_REQUIRE(Concepts::ExplicitlyConvertibleTo<Pointer<void>, Pointer<const char>>);
+
+        STATIC_REQUIRE(!Concepts::ImplicitlyConvertibleTo<Pointer<const void>, Pointer<char>>);
+        STATIC_REQUIRE(!Concepts::ImplicitlyConvertibleTo<Pointer<const void>, Pointer<const char>>);
+        STATIC_REQUIRE(!Concepts::ExplicitlyConvertibleTo<Pointer<const void>, Pointer<char>>);
+        STATIC_REQUIRE(Concepts::ExplicitlyConvertibleTo<Pointer<const void>, Pointer<const char>>);
+    }
+
+    SECTION("void pointer conversions")
+    {
+        Pointer<const char> p1 = "Hello";
+        Pointer<const void> p2 = p1;
+        Pointer<const char> p3 = static_cast<Pointer<const char>>(p2);
+        REQUIRE(p3);
+    }
+
+    SECTION("Pointer arithmetic")
+    {
+        Pointer<const char> s = "abcdef";
+        REQUIRE(*s == 'a');
+        REQUIRE(s[5] == 'f');
+        {
+            auto p = s + 1;
+            REQUIRE(*p == 'b');
+            ++p;
+            REQUIRE(*p == 'c');
+            p += 2;
+            REQUIRE(*p == 'e');
+            p -= 1;
+            REQUIRE(*p == 'd');
+            REQUIRE(*p-- == 'd');
+            REQUIRE(*p == 'c');
+            REQUIRE(*++p == 'd');
+        }
+    }
+}
