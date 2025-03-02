5891fe98d7614baf7b5290f6fd43d47713f19eeb
mocabe <hisa.mocabe@gmail.com>
2025-03-02 13:53:10 +0900

    Add BuildConfig

---
+++ Source/Core/Futurewalker.Core.BuildConfig.hpp
@@ -0,0 +1,34 @@
+﻿// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Futurewalker.Core.Primitive.hpp"
+#include "Futurewalker.Core.NonConstructible.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Detects build configurations.
+///
+class BuildConfig : NonConstructible
+{
+public:
+    static constexpr Bool IsDebug() noexcept;
+};
+
+///
+/// @brief Returns true if the library is being built in debug mode.
+///
+/// @note Do not assume this function depends on _NDEBUG macro.
+///
+constexpr Bool BuildConfig::IsDebug() noexcept
+{
+#if defined(_NDEBUG)
+    return false;
+#else
+    return true;
+#endif
+}
+}
+}
