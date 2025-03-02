c789815b2dffae4c87da33f92fc18868bae43580
mocabe <hisa.mocabe@gmail.com>
2025-03-02 13:58:33 +0900

    Add DataFunction

---
+++ Source/Core/Futurewalker.Core.DataFunction.hpp
@@ -0,0 +1,45 @@
+﻿// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Futurewalker.Core.Primitive.hpp"
+#include "Futurewalker.Core.Pointer.hpp"
+#include "Futurewalker.Core.NonConstructible.hpp"
+
+#include <cstring>
+
+namespace FW_DETAIL_NS 
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Data utilities.
+///
+class DataFunction final : NonConstructible
+{
+public:
+    template <class T, class U>
+    static void CopyMemory(Pointer<U> dst, Pointer<const T> src, const SInt64 size) noexcept;
+};
+
+///
+/// @brief Copy range of memory.
+///
+/// A wrapper of memcpy, which is easier to work with Pointer<T>.
+///
+/// @param[in] dst Destination memory location.
+/// @param[in] src Source memory location.
+/// @param[in] size Bytes to copy.
+///
+/// @note `dst` and `src` should be non-null.
+/// @note `size` should be greater than zero.
+///
+template <class T, class U>
+void DataFunction::CopyMemory(Pointer<U> dst, Pointer<const T> src, const SInt64 size) noexcept
+{
+    if (dst && src && size > 0)
+    {
+        std::memcpy(static_cast<U*>(dst), static_cast<const T*>(src), static_cast<size_t>(size));
+    }
+}
+}
+}
