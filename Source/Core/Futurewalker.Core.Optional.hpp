ab8114d701b6ad7b99692fb211b1e6343b671342
mocabe <hisa.mocabe@gmail.com>
2025-03-02 16:14:10 +0900

    Add Optional

---
+++ Source/Core/Futurewalker.Core.Optional.hpp
@@ -0,0 +1,103 @@
+﻿// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Futurewalker.Core.Primitive.hpp"
+#include "Futurewalker.Core.Exception.hpp"
+#include "Futurewalker.Core.TypeTraits.hpp"
+
+#include <optional>
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT 
+{
+///
+/// @brief Optional container class.
+///
+/// Contains either value of T or nothing.
+///
+template <class T>
+class Optional final : std::optional<T>
+{
+    using Base = std::optional<T>;
+
+public:
+    using std::optional<T>::optional;
+
+    ///
+    /// @brief Disable nullopt_t constructor.
+    ///
+    Optional(std::nullopt_t) = delete;
+
+    ///
+    /// @brief Returns true if the optional has a value.
+    ///
+    auto HasValue() const noexcept -> Bool
+    {
+        return Base::has_value();
+    }
+
+    ///
+    /// @brief Access value.
+    ///
+    /// @throw Exception if empty.
+    ///
+    template <class Self>
+    auto operator*(this Self&& self) -> decltype(auto)
+    {
+        if (self.HasValue())
+        {
+            return static_cast<TypeTraits::PropagateCVRef<decltype(self), Base>>(self).operator*();
+        }
+        throw Exception(ErrorCode::InvalidOperation);
+    }
+
+    ///
+    /// @brief operator->
+    ///
+    /// @throw Exception if empty.
+    ///
+    template <class Self>
+    auto operator->(this Self&& self) -> auto*
+    {
+        if (self.HasValue())
+        {
+            return static_cast<TypeTraits::PropagateCVRef<decltype(self), Base>>(self).operator->();
+        }
+        throw Exception(ErrorCode::InvalidOperation);
+    }
+
+    ///
+    /// @brief Emplace value.
+    ///
+    /// @tparam Args Argument types.
+    ///
+    /// @param args Arguments.
+    ///
+    template <class... Args>
+    auto Emplace(Args&&... args) -> void
+    {
+        Base::emplace(std::forward<Args>(args)...);
+    }
+
+    ///
+    /// @brief Reset.
+    ///
+    auto Reset() -> void
+    {
+        Base::reset();
+    }
+
+    ///
+    /// @brief Conversion operator to bool.
+    ///
+    explicit operator bool() const noexcept
+    {
+        return Base::operator bool();
+    }
+};
+
+template <class T>
+Optional(T) -> Optional<T>;
+}
+}
---
+++ Test/Core/Optional.cpp
@@ -0,0 +1,50 @@
+﻿#include <catch2/catch_all.hpp>
+
+#include <Futurewalker.Core.Optional.hpp>
+
+using namespace Futurewalker;
+
+TEST_CASE("Optional")
+{
+    SECTION("Default")
+    {
+        Optional<int> opt;
+        REQUIRE(!opt.HasValue());
+
+        opt = {};
+        REQUIRE(!opt.HasValue());
+    }
+
+    SECTION("int")
+    {
+        Optional<int> opt = 42;
+        REQUIRE(opt.HasValue());
+        REQUIRE(*opt == 42);
+
+        opt = 24;
+        REQUIRE(*opt == 24);
+
+        opt = Optional<int>();
+        REQUIRE(!opt.HasValue());
+    }
+
+    SECTION("vector<int>")
+    {
+        Optional<std::vector<int>> opt = {};
+        REQUIRE(!opt.HasValue());
+        opt = {};
+        REQUIRE(!opt.HasValue());
+
+        opt = std::vector {1, 2, 3};
+        REQUIRE(opt.HasValue());
+        REQUIRE(opt->size() == 3);
+
+        std::vector vec = {4, 5, 6, 7};
+        opt = vec;
+        REQUIRE(opt->size() == 4);
+
+        opt = std::move(vec);
+        REQUIRE(vec.empty());
+        REQUIRE(opt->size() == 4);
+    }
+}
