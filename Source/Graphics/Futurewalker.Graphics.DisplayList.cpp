3fd5f03fc9e91df65895b502c8f0a305e235bf43
mocabe <hisa.mocabe@gmail.com>
2025-03-09 18:12:05 +0900

    Add DisplayList

---
+++ Source/Graphics/Futurewalker.Graphics.DisplayList.cpp
@@ -0,0 +1,14 @@
+﻿// SPDX-License-Identifier: MIT
+
+#include "Futurewalker.Graphics.DisplayList.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief
+///
+DisplayList::~DisplayList() = default;
+}
+}
---
+++ Source/Graphics/Futurewalker.Graphics.DisplayList.hpp
@@ -0,0 +1,24 @@
+﻿// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Futurewalker.Graphics.DisplayListType.hpp"
+#include "Futurewalker.Graphics.Rect.hpp"
+#include "Futurewalker.Graphics.GraphicsUnits.hpp"
+
+#include "Futurewalker.Core.NonCopyable.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Container of recorded drawing commands.
+///
+class DisplayList : NonCopyable
+{
+public:
+    virtual ~DisplayList() = 0;
+    virtual auto GetBounds() const -> Rect<Dp> = 0;
+};
+}
+}
