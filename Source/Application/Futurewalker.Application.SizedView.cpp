8d9b89cdeb1a6302e29b33ea63e39272544b1258
mocabe <hisa.mocabe@gmail.com>
2025-05-10 17:35:05 +0900

    Initial implementation of SizedView

---
+++ Source/Application/Futurewalker.Application.SizedView.cpp
@@ -0,0 +1,169 @@
+﻿// SPDX-License-Identifier: MIT
+
+#include "Futurewalker.Application.SizedView.hpp"
+#include "Futurewalker.Application.ContainerView.hpp"
+#include "Futurewalker.Application.MeasureScope.hpp"
+#include "Futurewalker.Application.ArrangeScope.hpp"
+#include "Futurewalker.Application.ViewLayoutFunction.hpp"
+
+namespace FW_DETAIL_NS
+{
+///
+/// @brief Make instance.
+///
+auto SizedView::Make() -> Shared<SizedView>
+{
+    return View::MakeDerived<SizedView>();
+}
+
+///
+/// @brief Make with width and height.
+///
+auto SizedView::Make(Dp const width, Dp const height) -> Shared<SizedView>
+{
+    auto view = Make();
+    view->SetWidth(width);
+    view->SetHeight(height);
+    return view;
+}
+
+///
+/// @brief Make with content view.
+///
+auto SizedView::MakeWithContent(Shared<View> const& content) -> Shared<SizedView>
+{
+    auto view = Make();
+    view->SetContent(content);
+    return view;
+}
+
+///
+/// @brief Make with width/height and content view.
+///
+auto SizedView::MakeWithContent(Dp const width, Dp const height, Shared<View> const& content) -> Shared<SizedView>
+{
+    auto view = Make(width, height);
+    view->SetContent(content);
+    return view;
+}
+
+///
+/// @brief Set width.
+///
+auto SizedView::SetWidth(Dp const width) -> void
+{
+    if (Dp::IsNaN(width))
+    {
+        return;
+    }
+
+    const auto actual = Dp::Max(0, width);
+    if (_width != actual)
+    {
+        _width = actual;
+        InvalidateLayout();
+    }
+}
+
+///
+/// @brief Get width.
+///
+auto SizedView::GetWidth() const -> Dp
+{
+    return _width;
+}
+
+///
+/// @brief Set height.
+///
+auto SizedView::SetHeight(Dp const height) -> void
+{
+    if (Dp::IsNaN(height))
+    {
+        return;
+    }
+
+    const auto actual = Dp::Max(0, height);
+    if (_height != actual)
+    {
+        _height = actual;
+        InvalidateLayout();
+    }
+}
+
+///
+/// @brief Get height.
+///
+auto SizedView::GetHeight() const -> Dp
+{
+    return _height;
+}
+
+///
+/// @brief Set content view.
+///
+auto SizedView::SetContent(Shared<View> const& content) -> void
+{
+    if (_container)
+    {
+        _container->SetContent(content);
+    }
+}
+
+///
+/// @brief Get content.
+///
+auto SizedView::GetContent() -> Shared<View>
+{
+    if (_container)
+    {
+        _container->GetContent();
+    }
+    return {};
+}
+
+///
+/// @brief
+///
+SizedView::SizedView(PassKey<View> key)
+  : View(key)
+{
+}
+
+///
+/// @brief Initialize.
+///
+auto SizedView::Initialize() -> void
+{
+    _container = ContainerView::Make();
+    AddChildBack(_container);
+}
+
+///
+/// @brief Measure.
+///
+auto SizedView::Measure(MeasureScope& scope) -> void
+{
+    const auto& parameter = scope.GetParameter();
+    const auto& widthConstr = parameter.GetWidthConstraints();
+    const auto& heightConstr = parameter.GetHeightConstraints();
+
+    const auto measuredWidth = MeasureAxis(widthConstr, _width);
+    const auto measuredHeight = MeasureAxis(heightConstr, _height);
+    scope.SetMeasuredSize(measuredWidth, measuredHeight);
+
+    ForEachVisibleChild([&](View& view) { scope.MeasureChild(view, AxisConstraints::MakeExact(measuredWidth), AxisConstraints::MakeExact(measuredHeight)); });
+}
+
+///
+/// @brief Measure axis.
+///
+auto SizedView::MeasureAxis(const AxisConstraints& c, Dp const v) const -> Dp
+{
+    if (Dp::IsFinite(v))
+    {
+        return AxisConstraints::Constrain(c, ViewLayoutFunction::AlignToPixelGridByRound(v, *this));
+    }
+    return c.IsBounded() ? c.GetMax() : c.GetMin();
+}
+}
---
+++ Source/Application/Futurewalker.Application.SizedView.hpp
@@ -0,0 +1,47 @@
+﻿// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Futurewalker.Application.SizedViewType.hpp"
+#include "Futurewalker.Application.ContainerViewType.hpp"
+#include "Futurewalker.Application.View.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief A layout view which forces fixed width and height to its child.
+///
+class SizedView final : public View
+{
+public:
+    static auto Make() -> Shared<SizedView>;
+    static auto Make(Dp const width, Dp const height) -> Shared<SizedView>;
+    static auto MakeWithContent(Shared<View> const& content) -> Shared<SizedView>;
+    static auto MakeWithContent(Dp const width, Dp const height, Shared<View> const& content) -> Shared<SizedView>;
+
+    auto SetWidth(Dp const width) -> void;
+    auto GetWidth() const -> Dp;
+
+    auto SetHeight(Dp const height) -> void;
+    auto GetHeight() const -> Dp;
+
+    auto SetContent(Shared<View> const& content) -> void;
+    auto GetContent() -> Shared<View>;
+
+    SizedView(PassKey<View> key);
+
+protected:
+    auto Initialize() -> void override;
+    auto Measure(MeasureScope& scope) -> void override;
+
+private:
+    auto MeasureAxis(const AxisConstraints& c, Dp const v) const -> Dp;
+
+private:
+    Shared<ContainerView> _container;
+    Dp _width = 0;
+    Dp _height = 0;
+};
+}
+}
