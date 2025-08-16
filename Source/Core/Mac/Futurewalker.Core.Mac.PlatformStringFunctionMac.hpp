c4835c33ba3c9e9f0440d138d08bcfaf27a401eb
mocabe <hisa.mocabe@gmail.com>
2025-08-16 19:30:45 +0900

    Add macOS string utility

---
+++ Source/Core/Mac/Futurewalker.Core.Mac.PlatformStringFunctionMac.hpp
@@ -0,0 +1,27 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Core.Mac.Prelude.hpp"
+
+#include "Futurewalker.Core.NonConstructible.hpp"
+#include "Futurewalker.Core.String.hpp"
+
+#import <Foundation/Foundation.h>
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Platform specific string utilities. 
+///
+class PlatformStringFunctionMac : NonConstructible
+{
+public:
+    static auto ConvertNSStringToString(NSString* str) -> String;
+
+    __attribute__((ns_returns_retained)) 
+    static auto ConvertStringToNSString(StringView sv) -> NSString*;
+};
+}
+}
---
+++ Source/Core/Mac/Futurewalker.Core.Mac.PlatformStringFunctionMac.mm
@@ -0,0 +1,33 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Core.Mac.PlatformStringFunctionMac.hpp"
+
+#include "Futurewalker.Core.StringFunction.hpp"
+
+namespace FW_DETAIL_NS
+{
+///
+/// @brief Convert NSString to String.
+///
+auto PlatformStringFunctionMac::ConvertNSStringToString(NSString* str) -> String
+{
+    if (str)
+    {
+        return StringFunction::ConvertUtf8ToStringUnchecked([str UTF8String]);
+    }
+    return {};
+}
+
+///
+/// @brief Convert UTF-8 string to NSString.
+///
+__attribute__((ns_returns_retained))
+auto PlatformStringFunctionMac::ConvertStringToNSString(StringView sv) -> NSString*
+{
+    @autoreleasepool
+    {
+        auto const utf8Str = StringFunction::ConvertStringToStdString(sv);
+        return [NSString stringWithUTF8String:utf8Str.c_str()];
+    }
+}
+}
