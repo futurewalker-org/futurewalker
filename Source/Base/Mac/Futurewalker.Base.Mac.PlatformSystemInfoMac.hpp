f7dfa9ecd136d07fa13bcb3c582bbfffef4eca3a
mocabe <hisa.mocabe@gmail.com>
2025-08-16 19:36:27 +0900

    Add macOS PlatformSystemInfo implementation

---
+++ Source/Base/Mac/Futurewalker.Base.Mac.PlatformSystemInfoMac.hpp
@@ -0,0 +1,31 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Base.PlatformSystemInfo.hpp"
+#include "Futurewalker.Base.Locator.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief macOS implementation of PlatformSystemInfo.
+///
+class PlatformSystemInfoMac final : public PlatformSystemInfo
+{
+public:
+    auto IsWindows() const -> Bool override;
+    auto IsUnix() const -> Bool override;
+    auto IsMacOS() const -> Bool override;
+    auto IsIOS() const -> Bool override;
+    auto IsAndroid() const -> Bool override;
+};
+
+template <>
+struct Locator::Resolver<PlatformSystemInfoMac>
+{
+    using Interface = PlatformSystemInfo;
+    static auto Resolve() -> Shared<PlatformSystemInfoMac>;
+};
+}
+}
---
+++ Source/Base/Mac/Futurewalker.Base.Mac.PlatformSystemInfoMac.mm
@@ -0,0 +1,36 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Base.Mac.PlatformSystemInfoMac.hpp"
+
+namespace FW_DETAIL_NS
+{
+auto PlatformSystemInfoMac::IsWindows() const -> Bool
+{
+    return false;
+}
+
+auto PlatformSystemInfoMac::IsUnix() const -> Bool
+{
+    return false; // FIXME: Should this return true for macOS?
+}
+
+auto PlatformSystemInfoMac::IsMacOS() const -> Bool
+{
+    return true;
+}
+
+auto PlatformSystemInfoMac::IsIOS() const -> Bool
+{
+    return false;
+}
+
+auto PlatformSystemInfoMac::IsAndroid() const -> Bool
+{
+    return false;
+}
+
+auto Locator::Resolver<PlatformSystemInfoMac>::Resolve() -> Shared<PlatformSystemInfoMac>
+{
+    return Shared<PlatformSystemInfoMac>::Make();
+}
+}
