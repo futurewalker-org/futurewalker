ae0389e9ebb9fe02147680f1334ab141889622de
mocabe <hisa.mocabe@gmail.com>
2025-08-16 19:32:28 +0900

    Add macOS font manager

---
+++ Source/Graphics/Mac/Futurewalker.Graphics.Mac.PlatformSkiaFontManagerMac.hpp
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Graphics.PlatformSkiaFontManager.hpp"
+
+#include "Futurewalker.Base.Locator.hpp"
+
+#include <include/core/SkFontMgr.h>
+
+namespace FW_GRAPHICS_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+class PlatformSkiaFontManagerMac : public PlatformSkiaFontManager
+{
+public:
+    PlatformSkiaFontManagerMac();
+    auto FindTypefaceByFamilyNameAndStyle(String const& familyName, FontStyle const& style) -> Shared<Typeface> override;
+    auto GetSkFontMgr() -> sk_sp<SkFontMgr> override;
+
+private:
+    sk_sp<SkFontMgr> _fontMgr;
+};
+}
+}
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+template <>
+struct Locator::Resolver<Graphics::PlatformSkiaFontManagerMac>
+{
+    using Interface = Graphics::PlatformSkiaFontManager;
+    static auto Resolve() -> Shared<Graphics::PlatformSkiaFontManagerMac>;
+};
+}
+}
+
---
+++ Source/Graphics/Mac/Futurewalker.Graphics.Mac.PlatformSkiaFontManagerMac.mm
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Graphics.Mac.PlatformSkiaFontManagerMac.hpp"
+
+#include "Futurewalker.Graphics.SkiaTypeface.hpp"
+
+#include "Futurewalker.Core.StringFunction.hpp"
+
+#include <include/ports/SkFontMgr_mac_ct.h>
+
+namespace FW_GRAPHICS_DETAIL_NS
+{
+PlatformSkiaFontManagerMac::PlatformSkiaFontManagerMac()
+{
+    _fontMgr = SkFontMgr_New_CoreText(nullptr);
+}
+
+auto PlatformSkiaFontManagerMac::FindTypefaceByFamilyNameAndStyle(String const& familyName, FontStyle const& fontStyle) -> Shared<Typeface>
+{
+    if (_fontMgr)
+    {
+        auto const skFamilyName = StringFunction::ConvertStringToStdString(familyName);
+        auto const skFontStyle = FontStyleToSkFontStyle(fontStyle);
+        auto r = Shared<SkiaTypeface>::Make(_fontMgr->matchFamilyStyle(skFamilyName.c_str(), skFontStyle));
+        return r;
+    }
+    return {};
+}
+
+auto PlatformSkiaFontManagerMac::GetSkFontMgr() -> sk_sp<SkFontMgr>
+{
+    return _fontMgr;
+}
+}
+
+namespace FW_DETAIL_NS
+{
+auto Locator::Resolver<Graphics::PlatformSkiaFontManagerMac>::Resolve() -> Shared<Graphics::PlatformSkiaFontManagerMac>
+{
+    return Shared<Graphics::PlatformSkiaFontManagerMac>::Make();
+}
+}
