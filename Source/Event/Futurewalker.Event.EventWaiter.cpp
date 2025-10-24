3226efc83f400b7135a1e52c6e0ce9e58913faaa
mocabe <hisa.mocabe@gmail.com>
2025-10-25 02:37:39 +0900

    Add EventWaiter

---
+++ Source/Event/Futurewalker.Event.EventWaiter.cpp
@@ -0,0 +1,40 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Event.EventWaiter.hpp"
+
+#include "Futurewalker.Async.ThisThread.hpp"
+
+namespace FW_DETAIL_NS
+{
+///
+/// @brief Wait for next event.
+///
+auto EventWaiter::WaitEvent() -> Async<Event<>>
+{
+    co_return co_await ThisThread::Resume(_channel.Read());
+}
+
+///
+/// @brief Get tracker.
+///
+auto EventWaiter::GetTracker() -> Tracker&
+{
+    return *_tracker;
+}
+
+///
+/// @brief Get tracker.
+///
+auto EventWaiter::GetTracker() const -> Tracker const&
+{
+    return *_tracker;
+}
+
+///
+/// @brief Receive event.
+///
+auto EventWaiter::ReceiveEvent(Event<>& event) -> Async<Bool>
+{
+    co_return _channel.Write(event);
+}
+}
---
+++ Source/Event/Futurewalker.Event.EventWaiter.hpp
@@ -0,0 +1,53 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Event.EventWaiterType.hpp"
+#include "Futurewalker.Event.EventReceiver.hpp"
+
+#include "Futurewalker.Core.NonCopyable.hpp"
+
+#include "Futurewalker.Async.AsyncBroadcastChannel.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Event waiter.
+///
+class EventWaiter : NonCopyable
+{
+public:
+    template <class Receiver>
+    explicit EventWaiter(Receiver& receiver)
+    {
+        _tracker = Tracker::Make();
+        EventReceiver::Connect(receiver, *this, &EventWaiter::ReceiveEvent);
+    }
+
+    auto GetTracker() -> Tracker&;
+    auto GetTracker() const -> Tracker const&;
+
+    template <class T>
+    auto Wait() -> Async<Event<T>>
+    {
+        while (true)
+        {
+            auto const event = co_await WaitEvent();
+            if (event.Is<T>())
+            {
+                co_return event.As<T>();
+            }
+        }
+    }
+
+private:
+    auto WaitEvent() -> Async<Event<>>;
+    auto ReceiveEvent(Event<>& event) -> Async<Bool>;
+
+private:
+    Shared<Tracker> _tracker;
+    AsyncBroadcastChannel<Event<>> _channel;
+};
+}
+}
