ec2ff0663ec7aec3b466bc3de464043b184b31e9
mocabe <hisa.mocabe@gmail.com>
2025-11-03 22:59:32 +0900

    Add ThemeView

---
+++ Source/Application/Futurewalker.Application.ThemeView.cpp
@@ -0,0 +1,69 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Application.ThemeView.hpp"
+#include "Futurewalker.Application.Theme.hpp"
+#include "Futurewalker.Application.ContainerView.hpp"
+
+namespace FW_DETAIL_NS
+{
+auto ThemeView::Make() -> Shared<ThemeView>
+{
+    return View::MakeDerived<ThemeView>();
+}
+
+auto ThemeView::MakeWithContent(Shared<View> const& content) -> Shared<ThemeView>
+{
+    auto view = Make();
+    view->SetContent(content);
+    return view;
+}
+
+ThemeView::ThemeView(PassKey<View> key)
+  : View(key)
+{
+}
+
+auto ThemeView::GetContent() const -> Shared<View>
+{
+    if (_container)
+    {
+        return _container->GetContent();
+    }
+    return {};
+}
+
+auto ThemeView::SetContent(Shared<View> const& content) -> void
+{
+    if (_container)
+    {
+        _container->SetContent(content);
+    }
+}
+auto ThemeView::GetTheme() const -> Shared<Theme>
+{
+    return _theme;
+}
+
+auto ThemeView::SetTheme(Shared<Theme> const& theme) -> void
+{
+    if (_theme != theme)
+    {
+        _theme = theme;
+        UpdateTheme();
+    }
+}
+
+auto ThemeView::Initialize() -> void
+{
+    _container = ContainerView::Make();
+    AddChildBack(_container);
+}
+
+auto ThemeView::UpdateTheme() -> void
+{
+    if (_theme)
+    {
+        _theme->Apply(GetAttributeNode());
+    }
+}
+}
---
+++ Source/Application/Futurewalker.Application.ThemeView.hpp
@@ -0,0 +1,41 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Application.ThemeViewType.hpp"
+#include "Futurewalker.Application.ContainerViewType.hpp"
+#include "Futurewalker.Application.View.hpp"
+#include "Futurewalker.Application.ThemeType.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief
+///
+class ThemeView : public View
+{
+public:
+    static auto Make() -> Shared<ThemeView>;
+    static auto MakeWithContent(Shared<View> const& content) -> Shared<ThemeView>;
+
+    ThemeView(PassKey<View> key);
+
+    auto GetContent() const -> Shared<View>;
+    auto SetContent(Shared<View> const& content) -> void;
+
+    auto GetTheme() const -> Shared<Theme>;
+    auto SetTheme(Shared<Theme> const& theme) -> void;
+
+protected:
+    auto Initialize() -> void override;
+
+private:
+    auto UpdateTheme() -> void;
+
+private:
+    Shared<ContainerView> _container;
+    Shared<Theme> _theme;
+};
+}
+}
