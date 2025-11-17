43f64e8c3207ba9aa90f2eff069b8a812da43d83
mocabe <hisa.mocabe@gmail.com>
2025-11-18 00:00:08 +0900

    Add ActionView

---
+++ Source/Application/Futurewalker.Application.ActionView.cpp
@@ -0,0 +1,59 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Application.ActionView.hpp"
+#include "Futurewalker.Application.ContainerView.hpp"
+
+#include "Futurewalker.Action.CommandNode.hpp"
+
+namespace FW_DETAIL_NS
+{
+auto ActionView::Make() -> Shared<ActionView>
+{
+    return View::MakeDerived<ActionView>();
+}
+
+auto ActionView::MakeWithContent(Shared<View> contentView) -> Shared<ActionView>
+{
+    auto view = Make();
+    view->SetContent(contentView);
+    return view;
+}
+
+ActionView::ActionView(PassKey<View> key)
+  : View(key)
+{
+}
+
+auto ActionView::GetContent() -> Shared<View>
+{
+    if (_container)
+    {
+        return _container->GetContent();
+    }
+    return {};
+}
+
+auto ActionView::SetContent(Shared<View> content) -> void
+{
+    if (_container)
+    {
+        _container->SetContent(content);
+    }
+}
+
+auto ActionView::AddAction(CommandId const& command, Shared<Action> const& action) -> void
+{
+    CommandNode::AddAction(*this, command, action);
+}
+
+auto ActionView::RemoveAction(CommandId const& command) -> void
+{
+    CommandNode::RemoveAction(*this, command);
+}
+
+auto ActionView::Initialize() -> void
+{
+    _container = ContainerView::Make();
+    AddChildBack(_container);
+}
+}
---
+++ Source/Application/Futurewalker.Application.ActionView.hpp
@@ -0,0 +1,36 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Application.ActionViewType.hpp"
+#include "Futurewalker.Application.View.hpp" 
+#include "Futurewalker.Application.ContainerViewType.hpp"
+
+#include "Futurewalker.Action.CommandId.hpp"
+#include "Futurewalker.Action.ActionType.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+class ActionView : public View
+{
+public:
+    static auto Make() -> Shared<ActionView>;
+    static auto MakeWithContent(Shared<View> contentView) -> Shared<ActionView>;
+
+    ActionView(PassKey<View> key);
+
+    auto GetContent() -> Shared<View>;
+    auto SetContent(Shared<View> content) -> void;
+
+    auto AddAction(CommandId const& command, Shared<Action> const& action) -> void;
+    auto RemoveAction(CommandId const& command) -> void;
+
+protected:
+    auto Initialize() -> void override;
+
+private:
+    Shared<ContainerView> _container;
+};
+}
+}
