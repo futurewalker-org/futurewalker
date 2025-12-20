392e97d283443af3148b6454e77a96bcedceed1d
mocabe <hisa.mocabe@gmail.com>
2025-12-20 22:48:33 +0900

    Add KeyFunction

---
+++ Source/Application/Futurewalker.Application.KeyFunction.cpp
@@ -0,0 +1,31 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Application.KeyFunction.hpp"
+
+namespace FW_DETAIL_NS
+{
+///
+/// @brief Get modifier key strings.
+///
+/// @param modifiers modifier key flags.
+///
+/// @return Array of modifier key strings.
+///
+auto KeyFunction::GetModifierKeyStrings(ModifierKeyFlags const modifiers) -> std::vector<String>
+{
+    auto modifierString = std::vector<String>();
+    if ((modifiers & ModifierKeyFlags::Shift) != ModifierKeyFlags::None)
+    {
+        modifierString.push_back(String(u8"Shift"));
+    }
+    if ((modifiers & ModifierKeyFlags::Control) != ModifierKeyFlags::None)
+    {
+        modifierString.push_back(String(u8"Ctrl"));
+    }
+    if ((modifiers & ModifierKeyFlags::Alt) != ModifierKeyFlags::None)
+    {
+        modifierString.push_back(String(u8"Alt"));
+    }
+    return modifierString;
+}
+}
---
+++ Source/Application/Futurewalker.Application.KeyFunction.hpp
@@ -0,0 +1,23 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Application.KeyEventType.hpp"
+
+#include "Futurewalker.Core.String.hpp"
+#include "Futurewalker.Core.NonConstructible.hpp"
+
+#include <vector>
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief Key related functions.
+///
+struct KeyFunction : NonConstructible
+{
+    static auto GetModifierKeyStrings(ModifierKeyFlags const modifiers) -> std::vector<String>;
+};
+}
+}
