f521252aeed91a0ef78b8b162542d654a33f4576
mocabe <hisa.mocabe@gmail.com>
2025-12-22 00:22:03 +0900

    Add missing event definition

---
+++ Source/Application/Platform/Futurewalker.Application.PlatformHitTestEvent.cpp
@@ -0,0 +1,26 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Application.PlatformHitTestEvent.hpp"
+
+namespace FW_DETAIL_NS
+{
+auto PlatformHitTestEvent::GetPosition() const -> Point<Dp>
+{
+    return _position;
+}
+
+auto PlatformHitTestEvent::SetPosition(Point<Dp> const& position) -> void
+{
+    _position = position;
+}
+
+auto PlatformHitTestEvent::GetHit() const -> Bool
+{
+    return _hit;
+}
+
+auto PlatformHitTestEvent::SetHit(Bool const hit) -> void
+{
+    _hit = hit;
+}
+}
---
+++ Source/Application/Platform/Futurewalker.Application.PlatformHitTestEvent.hpp
@@ -0,0 +1,30 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Application.PlatformHitTestEventType.hpp"
+
+#include "Futurewalker.Event.EventParameter.hpp"
+
+#include "Futurewalker.Geometry.hpp"
+
+#include "Futurewalker.Unit.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+class PlatformHitTestEvent : public EventParameter
+{
+public:
+    auto GetPosition() const -> Point<Dp>;
+    auto SetPosition(Point<Dp> const& position) -> void;
+
+    auto GetHit() const -> Bool;
+    auto SetHit(Bool const hit) -> void;
+
+private:
+    Point<Dp> _position;
+    Bool _hit = false;
+};
+}
+}
