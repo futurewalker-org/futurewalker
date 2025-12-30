88bbbedf0254449ef9aa0a45d258626391b42257
mocabe <hisa.mocabe@gmail.com>
2025-12-30 14:36:13 +0900

    WIP: component gallery

---
+++ Example/ComponentGallery/ComponentGalleryApplication.hpp
@@ -0,0 +1,25 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include <Futurewalker.Application.Application.hpp>
+
+#include <Futurewalker.Action.CommandDispatcherType.hpp>
+
+namespace Futurewalker::ComponentGallery
+{
+class ComponentGalleryApplication : public Application
+{
+public:
+    static auto Make() -> Shared<ComponentGalleryApplication>;
+
+    ComponentGalleryApplication(PassKey<Application> key);
+
+protected:
+    auto Initialize() -> void override;
+    auto ReceiveEvent(Event<>& event) -> Async<Bool>;
+    auto ReceiveCommandEvent(Event<>& event) -> Async<Bool>;
+
+private:
+    Unique<CommandDispatcher> _commandDispatcher;
+};
+}
---
+++ Example/ComponentGallery/ComponentGalleryMain.cpp
@@ -0,0 +1,28 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "ComponentGalleryApplication.hpp"
+
+#include <Futurewalker.Base.Environment.hpp>
+
+#include <Futurewalker.Main.Main.hpp>
+
+#include <Futurewalker.Base.Debug.hpp>
+
+namespace Futurewalker
+{
+auto Main() -> Async<ExitCode>
+{
+    try
+    {
+        auto env = Environment();
+        Debug::Console();
+        auto app = ComponentGallery::ComponentGalleryApplication::Make();
+        co_await app->Run();
+    }
+    catch (...)
+    {
+        co_return ExitCode::Failure;
+    }
+    co_return ExitCode::Success;
+}
+}
