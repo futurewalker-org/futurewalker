48ce01e2923ebd30f0c55654f2af8916aa476e1f
mocabe <hisa.mocabe@gmail.com>
2025-12-30 14:32:07 +0900

    Add MenuBar

---
+++ Source/Component/Lamp/Futurewalker.Component.Lamp.MenuBar.cpp
@@ -0,0 +1,155 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Component.Lamp.MenuBar.hpp"
+#include "Futurewalker.Component.Lamp.PopupMenu.hpp"
+#include "Futurewalker.Component.Lamp.TextView.hpp"
+#include "Futurewalker.Component.Lamp.Style.hpp"
+#include "Futurewalker.Component.Lamp.PopupMenu.hpp"
+#include "Futurewalker.Component.Lamp.MenuView.hpp"
+#include "Futurewalker.Component.Lamp.MenuItemView.hpp"
+#include "Futurewalker.Component.Lamp.MenuItemButton.hpp"
+
+#include "Futurewalker.Application.FlexLayout.hpp" 
+#include "Futurewalker.Application.PaddingView.hpp"
+#include "Futurewalker.Application.MenuItem.hpp"
+
+namespace FW_LAMP_DETAIL_NS
+{
+auto MenuBar::Make() -> Shared<MenuBar>
+{
+    return View::MakeDerived<MenuBar>();
+}
+
+auto MenuBar::MakeWithMenu(Menu const& menu) -> Shared<MenuBar>
+{
+    auto view = Make();
+    view->SetMenu(menu);
+    return view;
+}
+
+MenuBar::MenuBar(PassKey<View> key)
+  : View(key)
+{
+}
+
+auto MenuBar::GetMenu() const -> Menu
+{
+    return _menu;
+}
+
+auto MenuBar::SetMenu(Menu const& menu) -> void
+{
+    _menu = menu;
+    BuildView();
+}
+
+auto MenuBar::Initialize() -> void
+{
+    EventReceiver::Connect(*this, *this, &MenuBar::ReceiveViewEvent);
+}
+
+auto MenuBar::ReceiveViewEvent(Event<>& event) -> Async<Bool>
+{
+    if (event.Is<MenuItemButtonEvent>())
+    {
+        auto const sender = event.As<MenuItemButtonEvent>()->GetSender();
+        if (event.Is<MenuItemButtonEvent::Down>())
+        {
+            if (auto const info = FindMenuItemInfo(sender))
+            {
+                CreatePopup();
+                UpdatePopup(*info);
+                co_return true;
+            }
+        }
+        else if (event.Is<MenuItemButtonEvent::Enter>())
+        {
+            if (auto const info = FindMenuItemInfo(sender))
+            {
+                UpdatePopup(*info);
+                co_return true;
+            }
+        }
+    }
+    co_return false;
+}
+
+auto MenuBar::ReceivePopupMenuEvent(Event<>& event) -> Async<Bool>
+{
+    if (event.Is<PopupMenuEvent::Closed>() || event.Is<PopupMenuEvent::Activated>())
+    {
+        DestroyPopup();
+    }
+    co_return false;
+}
+
+auto MenuBar::FindMenuItemInfo(Shared<View> const& button) const -> Optional<MenuItemInfo>
+{
+    for (auto const& info : _itemInfo)
+    {
+        if (info.button == button)
+        {
+            return info;
+        }
+    }
+    return {};
+}
+
+auto MenuBar::CreatePopup() -> void
+{
+    if (!_popup)
+    {
+        _popup = PopupMenu::Make();
+        EventReceiver::Connect(*_popup, *this, &MenuBar::ReceivePopupMenuEvent);
+    }
+}
+
+auto MenuBar::DestroyPopup() -> void
+{
+    _popup = nullptr;
+}
+
+auto MenuBar::UpdatePopup(MenuItemInfo const& info) -> void
+{
+    if (_popup && info.button)
+    {
+        auto const buttonRect = info.button->LocalToAncestorRect(info.button->GetContentRect(), GetSelf());
+        _popup->SetMenu(info.item.GetSubMenu());
+        _popup->SetAnchorRect(buttonRect);
+        _popup->SetAnchorEdge(PopupMenuAnchorEdge::Bottom);
+        _popup->SetAnchorAlignment(PopupMenuAnchorAlignment::Start);
+        _popup->Show(GetSelf());
+    }
+}
+
+auto MenuBar::BuildView() -> void
+{
+    if (_row)
+    {
+        _row->RemoveFromParent();
+        _row = nullptr;
+    }
+
+    FW_LOCAL_STATIC_ATTRIBUTE_DEFAULT_FUNCTION(EdgeInsets, AttributeItemPadding, [](auto p1, auto p2) { return p1.GetHorizontal() + p2.GetVertical(); }, Style::PaddingSmall, Style::PaddingExtraSmall);
+
+    auto row = FlexLayout::Make();
+    row->SetDirection(FlexLayoutDirection::Row);
+    row->SetMainAxisSize(FlexLayoutMainAxisSize::Max);
+    row->SetCrossAxisSize(FlexLayoutCrossAxisSize::Max);
+    row->SetCrossAxisAlignment(FlexLayoutCrossAxisAlignment::Center);
+    row->SetMainAxisAlignment(FlexLayoutMainAxisAlignment::Start);
+
+    _itemInfo.clear();
+    for (auto const& item : _menu.GetItems())
+    {
+        auto text = TextView::MakeWithText(item.GetTitle());
+        auto itemView = MenuItemView::MakeWithContents(nullptr, text, nullptr);
+        auto button = MenuItemButton::MakeWithContent(itemView);
+        AttributeNode::SetReference<&MenuItemViewStyle::Padding>(*button, AttributeItemPadding);
+        button->SetEnabled(item.GetSubMenu().GetItems().size() > 0);
+        row->AddChild(button);
+        _itemInfo.push_back(MenuItemInfo {item, button});
+    }
+    AddChildBack(row);
+}
+}
---
+++ Source/Component/Lamp/Futurewalker.Component.Lamp.MenuBar.hpp
@@ -0,0 +1,53 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Component.Lamp.MenuBarType.hpp"
+#include "Futurewalker.Component.Lamp.PopupMenuType.hpp"
+
+#include "Futurewalker.Application.View.hpp"
+#include "Futurewalker.Application.Menu.hpp"
+#include "Futurewalker.Application.MenuItem.hpp"
+#include "Futurewalker.Application.FlexLayoutType.hpp"
+
+#include <vector>
+
+namespace FW_LAMP_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+class MenuBar : public View
+{
+public:
+    static auto Make() -> Shared<MenuBar>;
+    static auto MakeWithMenu(Menu const& menu) -> Shared<MenuBar>;
+
+    MenuBar(PassKey<View> key);
+
+    auto GetMenu() const -> Menu;
+    auto SetMenu(Menu const& menu) -> void;
+
+protected:
+    auto Initialize() -> void override;
+    auto ReceiveViewEvent(Event<>& event) -> Async<Bool>;
+    auto ReceivePopupMenuEvent(Event<>& event) -> Async<Bool>;
+
+private:
+    struct MenuItemInfo
+    {
+        MenuItem item;
+        Shared<View> button;
+    };
+    auto FindMenuItemInfo(Shared<View> const& button) const -> Optional<MenuItemInfo>;
+    auto CreatePopup() -> void;
+    auto DestroyPopup() -> void;
+    auto UpdatePopup(MenuItemInfo const& info) -> void;
+    auto BuildView() -> void;
+
+private:
+    Menu _menu;
+    Shared<PopupMenu> _popup;
+    Shared<FlexLayout> _row;
+    std::vector<MenuItemInfo> _itemInfo;
+};
+}
+}
