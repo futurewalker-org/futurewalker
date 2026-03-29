7facb36f02aa1d2c4b3b0209522f1fcb4dcc9f12
mocabe <hisa.mocabe@gmail.com>
2026-03-29 12:39:46 +0900

    Add macOS implementation of PlatformMainThread

---
+++ src/Application/Mac/Futurewalker.Application.Mac.PlatformMainThreadMac.hpp
@@ -0,0 +1,30 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Application.PlatformMainThread.hpp"
+
+#include "Futurewalker.Base.Locator.hpp"
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief macOS implementation of the main thread.
+///
+class PlatformMainThreadMac : public PlatformMainThread
+{
+public:
+    auto IsMainThread() const -> Bool override;
+    auto Schedule() -> AsyncTask<void> override;
+    auto ScheduleAfter(const std::chrono::nanoseconds& delay) -> AsyncTask<void> override;
+};
+
+template <>
+struct Locator::Resolver<PlatformMainThreadMac>
+{
+    using Interface = PlatformMainThread;
+    static auto Resolve() -> Shared<PlatformMainThreadMac>;
+};
+}
+}
---
+++ src/Application/Mac/Futurewalker.Application.Mac.PlatformMainThreadMac.mm
@@ -0,0 +1,77 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "Futurewalker.Application.Mac.PlatformMainThreadMac.hpp"
+
+#import <AppKit/AppKit.h>
+
+#include <coroutine>
+
+namespace FW_DETAIL_NS
+{
+auto PlatformMainThreadMac::IsMainThread() const -> Bool
+{
+    @autoreleasepool
+    {
+        return [NSThread isMainThread];
+    }
+}
+
+auto PlatformMainThreadMac::Schedule() -> AsyncTask<void>
+{
+    struct Awaitable
+    {
+        auto await_ready()
+        {
+            return false;
+        }
+
+        auto await_suspend(std::coroutine_handle<> c)
+        {
+            @autoreleasepool
+            {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    c.resume();
+                });
+            }
+        }
+
+        auto await_resume()
+        {
+        }
+    };
+    co_await Awaitable();
+}
+
+auto PlatformMainThreadMac::ScheduleAfter(const std::chrono::nanoseconds& delay) -> AsyncTask<void>
+{
+    struct Awaitable
+    {
+        std::chrono::nanoseconds _delay;
+
+        auto await_ready()
+        {
+            return false;
+        }
+
+        auto await_suspend(std::coroutine_handle<> c)
+        {
+            @autoreleasepool
+            {
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, _delay.count()), dispatch_get_main_queue(), ^{
+                    c.resume();
+                });
+            }
+        }
+
+        auto await_resume()
+        {
+        }
+    };
+    co_await Awaitable(delay);
+}
+
+auto Locator::Resolver<PlatformMainThreadMac>::Resolve() -> Shared<PlatformMainThreadMac>
+{
+    return Shared<PlatformMainThreadMac>::Make();
+}
+}
