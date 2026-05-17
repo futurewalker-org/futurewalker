11a2bf8fd8216afbd316189c2ff942094b0d0023
mocabe <hisa.mocabe@gmail.com>
2026-05-17 22:32:42 +0900

    Add Uuid

---
+++ src/Core/Futurewalker.Core.Uuid.hpp
@@ -0,0 +1,27 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include "Futurewalker.Core.UuidType.hpp"
+
+#include "Futurewalker.Core.Optional.hpp"
+
+#include <array>
+
+#include <string_view>
+
+namespace FW_DETAIL_NS
+{
+namespace FW_EXPORT
+{
+///
+/// @brief UUID data type.
+///
+struct Uuid
+{
+    static auto MakeRandom() -> Uuid;
+    static auto MakeFromString(std::string_view const string) -> Optional<Uuid>;
+
+    std::array<uint8_t, 16> data = {};
+};
+}
+}
